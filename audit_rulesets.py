33852af11ca080cffd405fe91b76f34c56f59faa
Stephen Doiron <53412380+sdoiron0330@users.noreply.github.com>
2023-04-21 12:51:37 -0500

    .

--- audit_rulesets.py
+++ audit_rulesets.py
@@ -2,6 +2,7 @@ from nautobot_data_validation_engine.custom_validators import AuditRuleset, Audi
 
 class GitSiteAuditRuleset(AuditRuleset):
     model = "dcim.site"
+    enforce = True
 
     def audit(self):
         raise AuditError({"tenant": "git test"})
