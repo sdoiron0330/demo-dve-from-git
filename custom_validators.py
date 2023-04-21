d989667568041a269fbce32b5b52c805a058559a
Stephen Doiron <53412380+sdoiron0330@users.noreply.github.com>
2023-04-21 16:47:06 -0500

    .

--- custom_validators.py
+++ custom_validators.py
@@ -4,7 +4,21 @@ class GitSiteAuditRuleset(AuditRuleset):
     model = "dcim.site"
     enforce = True
 
-    def audit(self):
+    def audit_one(self):
+        raise AuditError({"region": "true"})
+
+    def audit_two(self):
         raise AuditError({"tenant": "git test"})
+
+    def audit(self):
+        messages = {}
+        for fn in [self.audit_one, self.audit_two]:
+            try:
+                fn()
+            except AuditError as ex:
+                messages.update(ex.messages_dict)
+        if messages:
+            raise AuditError(**messages)
+
 
 custom_validators = [GitSiteAuditRuleset]
