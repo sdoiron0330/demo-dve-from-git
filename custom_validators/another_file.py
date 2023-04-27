b1626b8cdfcc68e014982d58b4d323c2a33ccb7b
Stephen Doiron <53412380+sdoiron0330@users.noreply.github.com>
2023-04-27 15:43:29 -0500

    another file

--- custom_validators/another_file.py
+++ custom_validators/another_file.py
@@ -1,5 +1,6 @@
 from nautobot_data_validation_engine.custom_validators import DataComplianceRule
 
 class TestComplianceThing(DataComplianceRule):
+    model = "dcim.region"
     def audit(self):
         return
