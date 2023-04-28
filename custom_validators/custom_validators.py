a8ea8c9dd343957497b55e353318becb7d8fe3c8
Stephen Doiron <53412380+sdoiron0330@users.noreply.github.com>
2023-04-28 11:46:19 -0500

    .

--- custom_validators/custom_validators.py
+++ custom_validators/custom_validators.py
@@ -5,7 +5,7 @@ class GitSiteCompliance(DataComplianceRule):
     enforce = False
 
     def audit_one(self):
-        raise ComplianceError({"region": "true"})
+        return
 
     def audit_two(self):
         raise ComplianceError({"tenant": "git test"})
