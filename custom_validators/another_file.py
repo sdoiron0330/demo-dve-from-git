8ce8ef64c4226ca71b677dc110c6874237806064
Stephen Doiron <53412380+sdoiron0330@users.noreply.github.com>
2023-04-28 15:13:16 -0500

    .

--- custom_validators/another_file.py
+++ custom_validators/another_file.py
@@ -1,6 +1,11 @@
-from nautobot_data_validation_engine.custom_validators import DataComplianceRule
+from nautobot_data_validation_engine.custom_validators import DataComplianceRule, ComplianceError
 
 class TestComplianceThing(DataComplianceRule):
     model = "dcim.region"
     def audit(self):
         return
+
+class GitRackCompliance(DataComplianceRule):
+    model = "dcim.racks"
+    def audit(self):
+        raise ComplianceError({"name": "nope"})
