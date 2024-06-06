b62797b2200ee70e825185950b0835d0f5b62429
Stephen Doiron <53412380+sdoiron0330@users.noreply.github.com>
2024-06-06 10:58:59 -0500

    .'

--- hier_config_dynamic_remediations/test.py
+++ hier_config_dynamic_remediations/test.py
@@ -3,3 +3,14 @@
 def remediation(lineage):
     print("Hello World")
     print(lineage)
+    for line in lineage.all_children():
+        if line.text.startswith("no"):
+            line_items = line.text.split()
+            try:
+                if isinstance(int(line_items[1]), int):
+                    line_items.remove(line_items[1])
+            except ValueError:
+                pass
+        lineage.del_child_by_text(line.text)
+        lineage.add_child(" ".join(line_items))
+    return lineage
