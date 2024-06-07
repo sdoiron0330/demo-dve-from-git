ce9065494f23a60afa37efeb88ccf2bc15cd90be
Stephen Doiron <53412380+sdoiron0330@users.noreply.github.com>
2024-06-07 12:17:13 -0500

    .

--- hier_config_dynamic_remediations/test.py
+++ hier_config_dynamic_remediations/test.py
@@ -15,6 +15,15 @@ def remediation(lineage):
         if len(value) > 1:
             for line in value:
                 lineage.del_child_by_text(line)
+    for line in lineage.all_children():
+        line_items = line.text.split()
+        if line.startswith("no"):
+            index = 1
+        else:
+            index = 0
+        line_items.remove(line_items[index])
+        lineage.del_child_by_text(line.text)
+        lineage.add_child(" ".join(line_items))
     if len(list(lineage.all_children())) == 0:
         parent = lineage.parent
         parent.del_child_by_text(lineage.text)
