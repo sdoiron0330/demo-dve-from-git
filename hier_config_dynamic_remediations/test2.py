ed7e0ac70a3b19f060699435a322fe75a9245325
Stephen Doiron <53412380+sdoiron0330@users.noreply.github.com>
2024-06-26 15:31:23 -0500

    new file

---
+++ hier_config_dynamic_remediations/test2.py
@@ -0,0 +1,11 @@
+
+def remediation(remediation_config):
+    lineage = remediation_config.get_child("startswith", "snmp-server")
+    thing = []
+    for line in lineage.all_children():
+        new_line = line.text + "testing"
+        thing.append((line.text, new_line))
+    for delete, add in line:
+        lineage.del_child_by_text(delete)
+        lineage.add_child(add)
+    
