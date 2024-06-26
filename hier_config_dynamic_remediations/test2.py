2cdc1988c902f84a8c14f7c865e3b340c9958e3d
Stephen Doiron <53412380+sdoiron0330@users.noreply.github.com>
2024-06-26 15:43:12 -0500

    new file

--- hier_config_dynamic_remediations/test2.py
+++ hier_config_dynamic_remediations/test2.py
@@ -5,7 +5,7 @@ def remediation(remediation_config):
     for line in lineage.all_children():
         new_line = line.text + "testing"
         thing.append((line.text, new_line))
-    for delete, add in line:
+    for delete, add in thing:
         lineage.del_child_by_text(delete)
         lineage.add_child(add)
     
